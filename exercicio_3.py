ad68d590dbe9bdaf14617317fb3dbf79b060df91
eliasgusmao <74714823+eliasgusmao@users.noreply.github.com>
2023-05-16 11:23:47 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -11,5 +11,16 @@ C: Veículos com quatro rodas ou mais e com peso entre 3500 e 6000 kg;
 D: Veículos com quatro rodas ou mais e que acomodam mais de oito pessoas; E: Veículos com quatro rodas ou mais e com mais de 6000 kg. 
 '''
 
+qtd_rodas, peso, qtd_pessoas: 
 
+if qtd_rodas <= 3 
+print ('categoria a - Veículos com duas ou três rodas')
+if qtd_rodas = 4 and peso < 3500
+print ('categoria b - Veículos com quatro rodas, que acomodam até oito pessoas e seu peso é de até 3500 kg')
+if qtd_rodas = 4 and peso >= 3500 and qtd_pessoas < 8
+print ('categoria c - Veículos com quatro rodas ou mais e com peso entre 3500 e 6000 kg')
+if qtd_rodas = 4 and peso <= 3500 and qtd_pessoas > 8
+print ('categoria d - Veículos com quatro rodas ou mais e que acomodam mais de oito pessoas')
+if qtd_rodas > 4 
+print ('categoria e - Veículos com quatro rodas ou mais e com mais de 6000 kg')
 
